2b8252c610188bec941c2a3ad8790b08df0187a7
Seth Orell <seth.orell@gmail.com>
2023-09-11 19:36:29 -0500

    Replace old faker

--- test/sendMessage.e2e.test.ts
+++ test/sendMessage.e2e.test.ts
@@ -1,4 +1,4 @@
-import faker from 'faker';
+import { faker } from '@faker-js/faker';
 import EventBridge, { PutEventsRequest } from 'aws-sdk/clients/eventbridge';
 import { NewMessage } from '../src/models';
 
