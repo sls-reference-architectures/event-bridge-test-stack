c92110019948c4a3852adc77e9d16122b25ff80b
Seth Orell <seth.orell@gmail.com>
2024-09-05 08:38:56 -0500

    No global setup yet

--- jest.config.e2e.js
+++ jest.config.e2e.js
@@ -2,6 +2,6 @@ const baseConfig = require("./jest.config.js");
 
 module.exports = {
   ...baseConfig,
-  globalSetup: "./test/common/jest.setup.e2e.js",
+  // globalSetup: "./test/common/jest.setup.e2e.js",
   testTimeout: 120000,
 };
